69b9a3a5b8cb1fe2a3cc40ca74468abb97dd947c
Noopur Nanda <100906312+Noopur55@users.noreply.github.com>
2024-09-12 18:54:55 +0530

    Add files via upload

---
+++ frontendapp/static/frontendapp/js/script.js
@@ -0,0 +1,7 @@
+// document.addEventListener('DOMContentLoaded', function(){
+//   primaryField = document.getElementById('oarid');
+//   secondaryFiedlContainer = document.getElementById('fieldcontainer');
+//   secondaryField = document.getElementById('system');
+
+//   primaryField.addEventListener('input', function())
+// });
